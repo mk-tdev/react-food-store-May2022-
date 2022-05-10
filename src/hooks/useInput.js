efe1a0dd835d9732ef142c88939a71d6fcb70fb9
Muthu Kumar <mk-dev@outlook.com>
2022-05-11 00:49:35 +0800

    input-form-validation-hook-usereducer

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,28 +1,67 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
+
+const inputStateReducer = (state, action) => {
+  switch (action.type) {
+    case "INPUT_CHANGE":
+      return {
+        ...state,
+        value: action.payload,
+        isTouched: true,
+      };
+    case "INPUT_BLUR":
+      return {
+        ...state,
+        isTouched: true,
+      };
+    case "INPUT_RESET":
+      return {
+        ...state,
+        value: "",
+        isTouched: false,
+      };
+    default:
+      return state;
+  }
+};
+
+const initialInputState = {
+  value: "",
+  isTouched: false,
+};
 
 const useInput = (validateValue) => {
-  const [enteredValue, setEnteredValue] = useState("");
-  const [isTouched, setIsTouched] = useState(false);
+  // const [enteredValue, setEnteredValue] = useState("");
+  // const [isTouched, setIsTouched] = useState(false);
+  const [inputState, inputDispatch] = useReducer(
+    inputStateReducer,
+    initialInputState
+  );
 
-  const isValueValid = validateValue(enteredValue);
-  const hasError = !isValueValid && isTouched;
+  // const isValueValid = validateValue(enteredValue);
+  const isValueValid = validateValue(inputState.value);
+  // const hasError = !isValueValid && isTouched;
+  const hasError = !isValueValid && inputState.isTouched;
 
   const inputHandler = (e) => {
-    setEnteredValue(e.target.value);
-    setIsTouched(true);
+    // setEnteredValue(e.target.value);
+    // setIsTouched(true);
+    inputDispatch({ type: "INPUT_CHANGE", payload: e.target.value });
   };
 
   const blurHandler = () => {
-    setIsTouched(true);
+    // setIsTouched(true);
+    inputDispatch({ type: "INPUT_BLUR" });
   };
 
   const resetValues = () => {
-    setEnteredValue("");
-    setIsTouched(false);
+    // setEnteredValue("");
+    // setIsTouched(false);
+    inputDispatch({ type: "INPUT_RESET" });
   };
 
   return [
-    enteredValue,
+    // enteredValue,
+    inputState.value,
     inputHandler,
     blurHandler,
     hasError,
